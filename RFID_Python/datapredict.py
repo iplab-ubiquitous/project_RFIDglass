44164cba9d4586fcaee5142a4db54a1a10aaf214
Yuta Takayama <yprogdbcs@icloud.com>
2020-02-23 23:31:01 +0900

    混同行列のレイアウトを修正

--- RFID_Python/datapredict.py
+++ RFID_Python/datapredict.py
@@ -12,7 +12,7 @@ from Logput import Logput
 
 data_version = "0214_p01"  # テストデータのバージョン
 model_version = "0214_p02"  # モデルのバージョン
-modelType = "KNN"   # モデルの種類　[ SVC, KNN, RF, NN ]
+model_type = "KNN"   # モデルの種類　[ SVC, KNN, RF, NN ]
 
 dataset = pd.read_csv("./testData/testData_" + data_version + ".csv", header=None)
 
@@ -20,7 +20,7 @@ test_data = dataset.iloc[:, 0:6]
 test_label = dataset.iloc[:, 6]
 
 # 予測フェーズ
-clf = joblib.load(('./learningModel/test' + modelType + "_" + model_version + '.pkl'))
+clf = joblib.load(('./learningModel/test' + model_type + "_" + model_version + '.pkl'))
 pred = clf.predict(test_data)
 touch_true = test_label.tolist()
 print(pred)
@@ -29,20 +29,20 @@ c_matrix = confusion_matrix(touch_true, pred)
 print(c_matrix)
 labels = ["No-Touch", "eye-right", "eye-center", "eye-left", "cheek-right", "nose", "cheek-left"]
 cm_pd = pd.DataFrame(c_matrix, columns=labels, index=labels)
-fig, ax = plt.subplots(figsize=(9, 8))
-sns.heatmap(cm_pd, annot=True, cmap="Reds", ax=ax)
-plt.savefig('./confusionMatrix/result/png/confusion_matrix_data_' + modelType + "_" + model_version + '.png')
-with open('./confusionMatrix/result/csv/confusion_matrix_data_' + modelType + "_" + model_version + '.csv', 'w') as file:
+fig, ax = plt.subplots(figsize=(10, 3))
+sns.heatmap(cm_pd, annot=True, cmap="Oranges", ax=ax)
+plt.savefig('./confusionMatrix/result/png/confusion_matrix_data_' + model_type + "_" + model_version + '.png')
+with open('./confusionMatrix/result/csv/confusion_matrix_data_' + model_type + "_" + model_version + '.csv', 'w') as file:
     writer = csv.writer(file, lineterminator='\n')
     writer.writerows(cm_pd)
 print(classification_report(test_label, pred))
 print("正答率 = ", metrics.accuracy_score(test_label, pred))
 
-modellog = Logput(modelType)
+modellog = Logput(model_type)
 modellog.logput("program: datapredict.py\n")
-modellog.logput("Use model: test" + modelType + "_" + model_version + ".pkl\n")
+modellog.logput("Use model: test" + model_type + "_" + model_version + ".pkl\n")
 modellog.logput("TestData: testData_" + data_version + ".csv\n")
-modellog.logput('Save: confusion_matrix_data_' + modelType + "_" + model_version + '.csv\n')
-modellog.logput('Save: confusion_matrix_data_' + modelType + "_" + model_version + '.png\n')
+modellog.logput('Save: confusion_matrix_data_' + model_type + "_" + model_version + '.csv\n')
+modellog.logput('Save: confusion_matrix_data_' + model_type + "_" + model_version + '.png\n')
 modellog.logput("正答率： {}".format(metrics.accuracy_score(test_label, pred)))
 modellog.logput("\n")
